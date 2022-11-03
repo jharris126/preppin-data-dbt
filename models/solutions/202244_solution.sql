21c8edcb75cf0670497e798ede4b35c8c4b0c786
Jeremy Harris <jercharris89@gmail.com>
2022-11-03 09:19:19 -0700

    add 2022 week 44 solution

---
+++ models/solutions/202244_solution.sql
@@ -0,0 +1,30 @@
+with
+
+src_prep as (
+    select
+        string_split(Customer, ' ')[1][1] as order_id_prefix_1,
+        string_split(Customer, ' ')[-1][1] as order_id_prefix_2,
+        lpad("Order Number", 6, '0') as order_id_suffix,
+        "Order Number" as order_number,
+        Customer as customer,
+        case
+            when "Order Date" = '' then null
+            else cast(strptime("Order Date", '%d/%m/%Y') as date)
+        end order_date_1,
+        case
+            when "Date of Order" = '' then null
+            else cast(strptime("Date of Order", '%a %d %b %Y') as date)
+        end order_date_2,
+        case
+            when "Purchase Date" = '' then null
+            else cast(strptime("Purchase Date", '%d/%m/%Y') as date)
+        end order_date_3
+    from {{ source('preppin_data_input_files', 'src_week_202244') }}
+)
+
+select
+    concat(order_id_prefix_1, order_id_prefix_2, order_id_suffix) as order_id,
+    order_number,
+    customer,
+    concat(order_date_1, order_date_2, order_date_3) as order_date,
+from src_prep
