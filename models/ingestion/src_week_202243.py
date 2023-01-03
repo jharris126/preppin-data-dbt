eb6895670ad2844dcfb46c8d2328507a1c5ce21f
Jeremy Harris <jercharris89@gmail.com>
2023-01-02 21:32:29 -0800

    convert 202243 ingest to pandas with top level imports

--- models/ingestion/src_week_202243.py
+++ models/ingestion/src_week_202243.py
@@ -1,11 +1,5 @@
+from pandas import read_csv
+
 def model(dbt, session):
-    import requests
-    from io import BytesIO
-    from pyarrow.csv import read_csv
-
     source_data_url = "https://drive.google.com/u/0/uc?id=1lyYO70ooCldGLT8yPHoiiHn_z7S-CN7V&export=download"
-    csv_text = requests.get(source_data_url).text
-    csv_bytes = str.encode(csv_text)
-    csv_data = BytesIO(csv_bytes)
-
-    return read_csv(csv_data)
+    return read_csv(source_data_url)
